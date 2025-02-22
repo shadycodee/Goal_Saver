d3eec9f93718931a9b4ba7e9df7502983da32cbf
shadycodee <mikojohn158@gmail.com>
2025-02-22 16:51:54 +0800

    revamp Dashboard component with new title, input fields, and savings categories; add corresponding CSS styles

--- react/src/Pages/Dashboard.jsx
+++ react/src/Pages/Dashboard.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { TextField, Button } from '@mui/material';
+import '../styles/Dashboard.css';
 
 function Dashboard() {
   return (
     <div>
-      <h1>Goal Saver</h1>
+     <h1>What is your savings budget?</h1>
+     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+     <Button>Save</Button>
+
+     <div className='short-term-div'>
+        <h2>Short Term Savings</h2>
+        <Button>Add</Button>
+     </div>
+     <div className='medium-term-div'>
+      <h2>Medium Term Savings</h2>
+      <Button>Add</Button>
+     </div>
+     <div className='long-term-div'>
+      <h2>Long Term Savings</h2>
+      <Button>Add</Button>
+     </div>
     </div>
   )
 }
